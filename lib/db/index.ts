bc004cf2e18401ce4b9ca28864cf514e481bab59
xentriom <c04jason@gmail.com>
2026-03-20 16:34:48 -0400

    removed relations

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,7 +1,6 @@
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as relations from "@/drizzle/relations";
 import * as schema from "@/drizzle/schema";
 
 config({ path: ".env.local" });
@@ -13,4 +12,4 @@ if (!process.env.DATABASE_URL) {
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(process.env.DATABASE_URL, { prepare: false });
-export const db = drizzle(client, { schema: { ...schema, ...relations } });
+export const db = drizzle(client, { schema });
